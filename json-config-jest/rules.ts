723e2993ddb05b53d1c4cdf4e73ef7245ff4924e
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2023-10-03 18:27:01 -0500

    Include a new jest example based on the npm-based rules engine (#3)
    * Include a new jest example based on the npm-based rules engine
    
    Signed-off-by: Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
    
    * Exclude json-config-jest from deno fmt and lint
    
    Signed-off-by: Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>

---
+++ json-config-jest/rules.ts
@@ -0,0 +1,62 @@
+// fensak remove-start
+import type { ILineDiff, IPatch } from "@fensak-io/reng";
+// fensak remove-end
+
+function main(inp: IPatch[]): boolean {
+  const numPatches: number = inp.length;
+  if (numPatches == 0) {
+    // No files updated, so approve.
+    console.log("Accepting since no changes.");
+    return true;
+  } else if (numPatches > 1) {
+    // More than one file was updated, which means not just appversions.json was changed so reject.
+    console.log("Rejecting since more than one file was updated.");
+    return false;
+  }
+
+  const patch = inp[0];
+  if (patch.op != "modified") {
+    // The whole file was updated, so reject.
+    console.log("Rejecting since the whole file was updated.");
+    return false;
+  }
+  if (patch.diff.length > 1) {
+    // More than one section was updated, so reject.
+    console.log(
+      "Rejecting since more than one section of the file was updated.",
+    );
+    return false;
+  }
+  if (patch.path != "appversions.json") {
+    // Unexpected json file was updated, so reject.
+    console.log("Rejecting since the update was to an unexpected json file.");
+    return false;
+  }
+
+  const hunk = patch.diff[0];
+  const diffs = hunk.diffOperations.filter(function (d: ILineDiff) {
+    return d.op !== "untouched";
+  });
+  if (diffs.length == 0) {
+    // No real updates made to the file, so approve.
+    console.log("Accepting since no changes.");
+    return true;
+  } else if (diffs.length > 1) {
+    // More than one line was updated, so reject.
+    console.log("Rejecting since more than one line was updated.");
+    return false;
+  }
+
+  const d = diffs[0];
+  if (d.op !== "modified") {
+    // A line was inserted, or removed from the config, which means it's not just a change to a single app version
+    // config. Reject.
+    console.log(
+      "Rejecting since change was not a modification to an existing line.",
+    );
+    return false;
+  }
+
+  const subappUpdateRE = /^\s*"subapp": "v\d+\.\d+\.\d+",$/;
+  return subappUpdateRE.test(d.newText);
+}
