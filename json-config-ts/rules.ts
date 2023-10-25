d97ca9fe7d87dd65bbdcc471d7953f12b68eb2c4
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2023-10-25 10:06:02 -0500

    Update reng to 1.3.0 (#13)
    Signed-off-by: Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>

--- json-config-ts/rules.ts
+++ json-config-ts/rules.ts
@@ -3,7 +3,7 @@ import type {
   IChangeSetMetadata,
   ILineDiff,
   IPatch,
-} from "npm:@fensak-io/reng@^1.2.1";
+} from "npm:@fensak-io/reng@^1.3.0";
 // fensak remove-end
 
 // deno-lint-ignore no-unused-vars
